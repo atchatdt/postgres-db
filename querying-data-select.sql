0c49ce0ae0dc6cb5f7525ed7b5009d67a580ce4a
kuro <kisuvitinh@gmail.com>
2020-06-17 10:32:40 +0700

    comment ve select

--- querying-data-select.sql
+++ querying-data-select.sql
@@ -1,12 +1,16 @@
 /*
 
 Select 
-   - || nối chuỗi, hoặc concat
-   - Hạn chế dùng * khi select, nên lấy một vài cột cấn thiết
-   - *: lấy nhiều cột không cần thiết ảnh hưởng đến tốc độ thực thi
+- || nối chuỗi, hoặc concat
+- Hạn chế dùng * khi select, nên lấy một vài cột cấn thiết
+- *: lấy nhiều cột không cần thiết ảnh hưởng đến tốc độ thực thi
 Order by:
-   - Giá trị mặc định: ASC
-   
+- Giá trị mặc định: ASC
+DISTINCT:
+- Nên dùng chung với order by, tránh lộn xộn dữ liệu
+DISTINCT ON: Cân nhắn nên dùng này khi muốn chọn nhiều thuộc tính
+- mà giá trị đầu tiên là duy nhất
+Không dùng DISTINCT ON:  khi DISTINCT nhiều cột, nó sẽ lựa chọn những cặp không trùng lạp
 */
 
 SELECT
@@ -42,7 +46,18 @@ ORDER BY
 	LENGTH(first_name) DESC;
 
 
--- 
+-- DISTINCT
+SELECT * FROM t1
+
+SELECT
+	DISTINCT bcolor,
+	fcolor
+FROM
+	t1
+ORDER BY
+	bcolor,
+	fcolor
+
 SELECT * FROM t1
 SELECT
 	DISTINCT ON
